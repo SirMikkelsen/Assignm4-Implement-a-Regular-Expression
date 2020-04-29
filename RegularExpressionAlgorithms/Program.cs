8e6c8d13ec31784d7ab605662b646667e79e5783
Gordon <gordonmikkelsen87@gmail.com>
2020-04-29 17:37:50 +0200

    createt project

---
+++ RegularExpressionAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegularExpressionAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
