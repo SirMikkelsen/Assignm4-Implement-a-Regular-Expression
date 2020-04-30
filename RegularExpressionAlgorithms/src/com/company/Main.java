444313621a22cc3c41b13902f553a8f2a3d0c361
Gordon <gordonmikkelsen87@gmail.com>
2020-04-30 16:50:33 +0200

    Added all classes

---
+++ RegularExpressionAlgorithms/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import com.company.algorithm.Bag;
+import com.company.algorithm.Digraph;
+import com.company.algorithm.DirectedDFS;
+import com.company.algorithm.NFA;
+
+import java.util.Scanner;
+
+import static sun.java2d.cmm.ColorTransform.In;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // First get some text, then some regex
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter text: ");
+        String text = scanner.next();
+        System.out.println("Enter regex: ");
+        String regex = scanner.next();
+
+    }
+}
